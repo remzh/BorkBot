e620e1f6b4c566587428d4629eb74b724c312728
Ryan Zhang <ryanzh778@gmail.com>
2019-06-27 14:52:50 -0600

    Final (possibly) commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# BorkBot
-Bork bork bork
+# Bork
+
+![Bork bork!](https://ryan778.github.io/images/bork.png)
+Discord is for you and your friends. But wait, this is the internet! You don't have any friends! (/s...?)
+This is where Bork comes in. He'll be your friend. Friend...?
+
+## About Bork
+![Discord Hack Week](https://cdn-images-1.medium.com/max/2560/1*lh6NS8hx0pu5mlZeSqnu5w.jpeg)
+Bork is a cat for Discord Hack Week. Why isn't he a dog? Good question, I don't know. I just like cats?
+It's also a common(?) saying that dogs tend to think of us as gods, while cats think of themselves of gods. You better pay attention to Bork!
+
+To ensure this, Bork has a multitude of "features" that help ensure you'll never forget about Bork. Bork will bork to you, you'll have to bork back to Bork, Bork will sometimes bork in French, and a few other tidbits.
+
+**Why "Bork"?**
+It's an inside joke. Bork sounds like "bark", but bork is most definitely a cat. 
+Bork's also both a verb and a noun, which means this readme will be extra confusing for everyone (see above). Yay!
+
+## Features
+
+### Talk to and/or about Bork!
+If you don't use "bork" in your messages at least every six messages, Bork won't let you talk until you do. You won't ever forget him! (Using commands count as "borking" to bork)
+If you're good friends with Bork (>=100 friendship), then you only have to use "bork" every twelve messages. 
+
+### Random Borks
+Everytime someone says anything, there's a relatively small (8%) chance that Bork will bork up and say "bork". 
+If Bork is in a voice channel (see commands), he'll also meow randomly. 
+
+### French Bork
+Bork is fluent in French, and will on occasion (10%) translate your messages into French. 
+If Bork doesn't like you (<0 friendship), then he'll also delete your message so your other friends won't understand you (unless they speak French, of course)
+
+### Bork Loves You
+If you get your friendship with Bork to 1,000, then he'll love you. Forever. That's right, once you reach 1,000, then the friendship can no longer go down no matter how many times you poke or slap him (trying to stab him will still make him retaliate). Other than that, not much else happens. 
+Potentially a reference to [Stardew Valley](https://stardewvalleywiki.com/Animals#Cat_or_Dog)
+
+### Commands
+All commands are prefixed with **bork**. Who needs random and confusing symbols?
+
+`bork bork`: bork! (replacement for ping)
+`bork status`: shows your friendship status (with pictures!!)
+
+`bork vc`: joins bork to your voice channel. He'll meow randomly. 
+`bork lvc`: makes bork leave the voice channel, can be used by anyone. Decreases friendship because Bork doesn't like that (-3). 
+
+`bork pet`: pets Bork. Increases friendship (+5). 5 minute cooldown. 
+`bork poke`: pokes Bork. Decreases friendship (-5). 5 minute cooldown. 
+`bork slap`: slaps Bork. Significantly decreases friendship (-20). 10 minute cooldown. 
+
+`bork stab`: attempts to stab Bork. If you succeed, Bork will die and leave the server. However, if you fail, you'll be kicked from the server. Note that Bork is very aggressive when need be, which is why there's a 98% chance that you'll be the one kicked.
+
+## Self-hosting
+- You'll need to have NodeJS on the machine you're running Bork. 
+- Download this repository, and run `npm install` to install the necessary packages. 
+- Voice channel features require ffmpeg. Download a copy of ffmpeg and put it in the bot directory, DiscordJS should automatically detect and use it. It's somewhat finicky, but it's also a rather minor component. 
+- If you want to use my profile picture of Bork, upload `images/happy.png` as the profile picture. 
+- Put your bot token into token.json, and start the bot with `node index.js`!
+
+## Disclaimers
+- Bork is not intended to be used on production servers for (hopefully) obvious reasons. 
+- Bork stores friendship data in memory. If you stop the bot, you'll lose friendship values. 
+- There's no guarantee that the bot won't spontaneously combust, despite the (quite possibly excessive) amount of effort I put into making a (near) useless bot and a fancy readme file. 
+
+### Third-Party Libraries
+- Bork images are from [Freepik](https://www.freepik.com/free-vector/flat-pack-cute-cat-emojis_1001312.htm) and are licensed under Creative Commons. 
+- The meow sound is from [FreeSounds](https://freesound.org/people/tuberatanka/sounds/110011/) and is licensed under CC0
+- This bot uses [DiscordJS](https://discord.js.org/#/) because I don't have the time to write [my own freaking batch API wrapper](https://github.com/Mad0Max/discord.bat) (but wouldn't it be cool to make it out of Batch?)
--- index.js
+++ index.js
@@ -15,24 +15,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client(); 
 const http = require('http').Server(app);
 
-const { PerformanceObserver, performance } = require('perf_hooks'); //For eval command
-const {VM} = require('vm2');
-const vm = new VM({
-  timeout: 500, 
-  sandbox: {
-    console: {
-      log: (d) => {ext_secureLog(d)}
-    }
-  }
-});
-let vm_log = '';
-function ext_secureLog(data) {
-  if(typeof data === 'object'){data = JSON.stringify(data)}
-  vm_log += `> ${data}\n`; 
-};
-
-
-
 client.on('ready', () => {//Initialize Discord connection
   console.log(`[Bork ${botVer}] Logged in as ${client.user.tag}!`);
   client.user.setActivity('Bork Bork', { type: 'PLAYING' });
@@ -53,31 +35,145 @@ function getParams(msg, sp){
 	return msg.split(' ');
 }
 
-function randInt(min, max){
-	return Math.round(Math.random()*(max-min))+min; 
+let borkCount = {
+
 }
+
+let friendshipStatus = {
+
+}
+
+let cooldowns = {
+
+}
+
+let voiceConnection; //For voice input
 
 client.on('message', msg => {
 if(msg.author.id === client.user.id || msg.author.bot){return}  // Don't reply to self, don't reply to bots
-  
+ 
+if(Math.random() < 0.08) {msg.channel.send('bork')}
+
 let mf = msg.content; //message formatted
+
+if(mf.toLowerCase().indexOf('bork') !== -1){
+  borkCount[msg.author.id] = 0; 
+}
+else{
+  if(borkCount[msg.author.id]){
+    borkCount[msg.author.id] ++; 
+  }
+  else{
+    borkCount[msg.author.id] = 1; 
+  }
+}
+
+if(!friendshipStatus[msg.author.id]){
+  friendshipStatus[msg.author.id] = 0; 
+}
+
+if(borkCount[msg.author.id] >= 6 && friendshipStatus[msg.author.id] < 100 || borkCount[msg.author.id] >= 12){
+  msg.delete(); 
+  msg.channel.send('bORK! *(you\'re not paying enough attention to bork! use his name more!)*').then(m => m.delete(5000));
+}
+
 if(mf.slice(0, cmdPrefix.length).toLowerCase() === cmdPrefix){//Command
   mf = mf.slice(cmdPrefix.length);
   if(mf.slice(-1) === ' '){mf = mf.slice(0, -1)} //Trim off extra space
   let mspl = mf.split(' '); //message split (as commands generally work like this)
   logCmd(msg);
   switch(mspl[0].toLowerCase()){  
-    case 'ping': 
-      msg.reply(`:ping_pong: Pong! (*Time: ${Date.now() - msg.createdTimestamp}ms*)`);
+    case 'bork': 
+      msg.channel.send(`:cat: Bork! (*${Date.now() - msg.createdTimestamp}ms*)`);
       break;
-    case 'sp:killswitch':
-      msg.channel.send(`**Initiating emergency kill switch...**\n*Note that this will terminate the connection between the bot and the Discord server, and the bot will no longer be able to respond to anything. This action is logged to prevent abuse.*`); 
-      console.log(`Terminating Discord connection... (Initiated by ${msg.author.username} (id: ${msg.author.id}))`);
-      client.destroy(); 
+    case 'pet': 
+      if(Date.now() > cooldowns[msg.author.id+'-pet'] + 300000 || !cooldowns[msg.author.id+'-pet']){
+        msg.channel.send(`Bork!`); 
+        friendshipStatus[msg.author.id] += 5; 
+        cooldowns[msg.author.id+'-pet'] = Date.now(); 
+      }
+      else{
+        msg.channel.send(`Too many pets!`)}
+      break;
+    case 'poke': 
+      if(Date.now() > cooldowns[msg.author.id+'-poke'] + 300000 || !cooldowns[msg.author.id+'-poke']){
+        msg.channel.send(`bOrk!`); 
+        friendshipStatus[msg.author.id] -= 5; 
+        cooldowns[msg.author.id+'-poke'] = Date.now(); 
+      }
+      else{
+        msg.channel.send(`Too many pokes!`)}
+      break;
+    case 'slap': 
+      if(Date.now() > cooldowns[msg.author.id+'-slap'] + 600000 || !cooldowns[msg.author.id+'-slap']){
+        msg.channel.send(`bORk!!!!`); 
+        friendshipStatus[msg.author.id] -= 20; 
+        cooldowns[msg.author.id+'-slap'] = Date.now(); 
+      }
+      else{
+        msg.channel.send(`Too many slaps!`)}
+      break;
+    case 'status': 
+      let frs = friendshipStatus[msg.author.id]; 
+      msg.reply(`you have ${(frs < 1000?frs:'∞')} friendship points with Bork!`);
+      if(frs < -30){
+        msg.channel.sendFile(path.join(__dirname, 'images', 'mad.png'))}
+      else if(frs < 0){
+        msg.channel.sendFile(path.join(__dirname, 'images', 'unhappy.png'))}
+      else if(frs < 100){
+        msg.channel.sendFile(path.join(__dirname, 'images', 'happy.png'))}
+      else{
+        msg.channel.sendFile(path.join(__dirname, 'images', 'very happy.png'))}
+      break; 
+    case 'vc':
+      if(msg.member.voiceChannel){
+        msg.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          voiceConnection = connection;
+          msg.reply('borked into a borking channel!');
+        })
+        .catch(console.log);
+      }
+      else{
+        msg.reply('you need to bork into a borking channel first!!')
+      }
+      break;
+    case 'lvc': 
+      if(voiceConnection){
+        voiceConnection.disconnect(); 
+        delete voiceConnection;
+        msg.channel.send(`Borked out of the borking channel!`);
+      }
       break;
   }
 }
+else{
+  if(mf.length > 4){
+    if(Math.random() > 0.9){
+      translate(mf, {from: 'auto', to: 'fr'}).then(res => {
+        if(friendshipStatus[msg.author.id] < 0){
+          msg.delete() }
+        msg.channel.send(`*(from ${msg.author.username})* ${res.text}`);
+      })
+    }
+  }
+}
+
+
+if(friendshipStatus[msg.author.id] >= 1000 && friendshipStatus[msg.author.id] < 2000){
+  friendshipStatus[msg.author.id] = 10000000; // close enough to infinity, using "Infinity" can cause issues
+  msg.reply('Bork loves you ♡')
+}
+
 });
+
+setInterval(function(){ // random meowing
+  if(voiceConnection){
+    if(Math.random() > 0.92){
+      voiceConnection.playFile(path.join(__dirname, 'meow.mp3'));
+    }
+  }
+}, 3000); 
 
 client.on('error', console.error); // Placeholder for now, will be properly handled later
 
